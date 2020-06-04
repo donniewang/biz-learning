da79c8f093dd0b7bdfa4169d4fa00b0b525c10cb
donniewang <DonnieWang@qq.com>
2020-06-04 15:52:57 +0800

    add config client

--- config-client/src/test/java/com/biz/cloud/ConfigClientApplicationTests.java
+++ config-client/src/test/java/com/biz/cloud/ConfigClientApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ZuulProxyApplicationTests {
+class ConfigClientApplicationTests {
 
 	@Test
 	void contextLoads() {
